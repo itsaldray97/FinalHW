5a61a1fe0530a8d86d0d26c46115d6ccfc603525
itsaldray97 <raysetia@seas.upenn.edu>
2025-12-05 23:24:26 -0700

    first commit

--- bridge.py
+++ bridge.py
@@ -51,7 +51,7 @@ def scan_blocks(chain, contract_info="contract_info.json"):
     ).hex()
 
     UNWRAP_TOPIC = "0x" + w3.keccak(
-        text="Withdraw(address,address,uint256)"
+        text="unwrap(address,address,uint256)"
     ).hex()
 
     # ============================================================
